b9327f079f5eaf280c28d35c69536e2b8e58e547
julian <kontakt@j-petri.de>
2024-06-30 23:00:59 +0200

    migrated android-image-cropper to fork

--- ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/fragment/crop/CropFragment.java
+++ ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/fragment/crop/CropFragment.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import androidx.annotation.ColorRes;
 import androidx.core.content.ContextCompat;
 
-import com.theartofdev.edmodo.cropper.CropImageView;
+import com.canhub.cropper.CropImageView;
 
 import org.jetbrains.annotations.NotNull;
 
